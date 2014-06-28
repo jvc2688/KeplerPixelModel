77e25160b48079d2ad043875e4cae9b668f8feab
Dun Wang <dw1519@nyu.edu>
2014-06-28 00:05:47 -0400

    train-test and regularization to the distortion

--- code/magNeighor/magNeighorFit.py
+++ code/magNeighor/magNeighorFit.py
@@ -1,6 +1,8 @@
 import kplr
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
 import leastSquareSolver as lss
 
 client = kplr.API()
@@ -109,24 +111,28 @@ def load_data(tpf):
     print('time:%f'%(time[2]-time[1]))
     return time, flux, pixel_mask, kplr_mask
 
-def get_train_mask(target_flux, percent=0.1):
-    train_mask = np.ones(target_flux.shape[0])
-    length = int(target_flux.shape[0] * percent)
-    initial = int(target_flux.shape[0] * (0.5-percent/2.0))
-    print length
-    train_mask[initial:initial+length] = 0
+def get_train_mask(target_flux, percent=0.1, specify=False, initial=0, length=0):
+    if specify:
+        train_mask = np.ones(target_flux.shape[0])
+        train_mask[initial:initial+length] = 0
+    else:
+        train_mask = np.ones(target_flux.shape[0])
+        length = int(target_flux.shape[0] * percent)
+        initial = int(target_flux.shape[0] * (0.5-percent/2.0))
+        print length
+        train_mask[initial:initial+length] = 0
     return train_mask
 
 def get_fake_data(target_flux, position, length):
     #the sine distortion
-    '''
+    
     factor = np.arange(target_flux.shape[0])
     factor = (1+0.004*np.sin(12*np.pi*factor/factor[-1]))
     for i in range(0, target_flux.shape[0]):
         target_flux[i] = target_flux[i] * factor[i]
-    '''
+    
     #the fake transit
-    target_flux[position:position+length, :] = target_flux[position:position+length, :]*(1+0.004)
+    #target_flux[position:position+length, :] = target_flux[position:position+length, :]*(1+0.004)
     return target_flux
 
 #fit the target kic with the pixels of the neighors in magnitude
@@ -148,9 +154,10 @@ def neighor_fit(kic, quarter, neighor_num=1, offset=0, ccd=True, l2=0, plot=True
     
     neighor_flux_matrix = np.float64(np.concatenate(neighor_fluxes, axis=1))
 
-    #target_flux = get_fake_data(target_flux, 2000, 20)
+    target_flux = get_fake_data(target_flux, 2000, 20)
 
     train_mask = get_train_mask(target_flux, Percent)
+    #train_mask = get_train_mask(target_flux, Percent, True, 1986, 68)
     target_train_set = target_flux[train_mask>0, :]
     neighor_train_set = neighor_flux_matrix[train_mask>0, :]
     target_test_set = target_flux[train_mask==0, :]
@@ -208,19 +215,21 @@ def neighor_fit(kic, quarter, neighor_num=1, offset=0, ccd=True, l2=0, plot=True
         plt.setp( axes[1].get_xticklabels(), visible=False)
         plt.setp( axes[1].get_yticklabels(), visible=False)
         axes[1].set_ylabel("flux of fit")
-
+        
+        ratio = np.divide(target_flux, fit_flux)
+        result.append(np.mean(ratio[2000:2020, :], axis=0)/1.004)
         axes[2].plot(time, np.divide(target_flux[:, Pixel], fit_flux[:, Pixel]))
         axes[2].set_ylim(0.990,1.010)
         axes[2].set_xlabel("time")
         axes[2].set_ylabel("ratio of data and fit")
-        #sin = 1+0.004*np.sin(12*np.pi*(time-time[0])/(time[-1]-time[0]))
-        #axes[2].plot(time, sin, color='red')
+        sin = 1+0.004*np.sin(12*np.pi*(time-time[0])/(time[-1]-time[0]))
+        axes[2].plot(time, sin, color='red')
         #axes[2].axhline(y=1.004,xmin=0,xmax=3,c="red",linewidth=0.5,zorder=0)
 
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                             wspace=0, hspace=0)
         plt.suptitle('Kepler %d Quarter %d Pixel(%d,%d) \n Fit Source[Initial:%d Number:%d CCD:%r] RMS Deviation:%f'%(origin_tpf.ktc_kepler_id, origin_tpf.sci_data_quarter, pixel_y, pixel_x, offset+1, neighor_num, ccd, result[2][Pixel]))
-        plt.savefig('fit(%d,%d)_%d_%d_ccd%r.png'%(pixel_y, pixel_x, offset+1,neighor_num,ccd))
+        plt.savefig('fit(%d,%d)_%d_%d_ccd%rTran_reg%.0eSin.png'%(pixel_y, pixel_x, offset+1,neighor_num,ccd, l2))
         plt.clf()
 
     #print the fitting coefficient
@@ -248,6 +257,21 @@ def neighor_fit(kic, quarter, neighor_num=1, offset=0, ccd=True, l2=0, plot=True
     return result
 
 if __name__ == "__main__":
+    if False:
+        neighor_num = np.arange(20)+1
+        ratio = np.empty_like(neighor_num, dtype=float)
+        for i in neighor_num:
+            result = neighor_fit(5088536, 5, i, 0, True, 0)
+            ratio[i-1] = result[4][Pixel]
+            neighor_num[i-1] = result[0].shape[0]
+        plt.plot(neighor_num,ratio,'bs')
+        plt.title('test-train')
+        plt.xlabel("Number of parameters")
+        plt.ylabel("ratio of the fitting and true singal")
+        #plt.ylim(ymax=1)
+        plt.ylim(0.99, 1.01)
+        plt.savefig('paraNum-ratio_pixel(3,4)Train.png')
+
     if False:
         neighor_num = np.arange(12)+1
         residuals = np.empty_like(neighor_num, dtype=float)
@@ -277,7 +301,23 @@ if __name__ == "__main__":
         neighor_fit(5088536, 5, 1, 0, False)
 
     if True:
-        neighor_fit(5088536, 5, 5, 0, True, 0)
+        result = neighor_fit(5088536, 5, 5, 0, True, 0, True)
+
+    if False:
+        num = 5
+        strength = np.arange(8)
+        ratio = np.empty_like(strength, dtype=float)
+        for i in strength:
+            result = neighor_fit(5088536, 5, num, 0, True, (1e3)*(10**strength[i]), True)
+            ratio[i] = result[4][Pixel]
+            strength[i] += 3
+        plt.clf()
+        plt.plot(strength, ratio, 'bs')
+        plt.title('Number of stars:%d'%num)
+        plt.xlabel(r'Strength of Regularization($log \lambda$)')
+        plt.ylabel("ratio of the fitting and true singal")
+        plt.ylim(ymax=1.0)
+        plt.savefig('l2-ratio_num%d_pixel(3,4).png'%num)
 
     if False:
         strength = np.arange(7)
@@ -292,3 +332,27 @@ if __name__ == "__main__":
         plt.ylabel("RMS Deviation")
         plt.ylim(ymin=0)
         plt.savefig('l2-rms.png')
+
+    if False:
+        neighor_num = np.arange(5)+1
+        para_num = np.empty_like(neighor_num)
+        print neighor_num
+        strength = np.arange(5)
+        ratio = np.empty((strength.shape[0], neighor_num.shape[0]), dtype=float)
+        for i in strength:
+            for j in neighor_num:
+                print(i, j)
+                result = neighor_fit(5088536, 5, j, 0, True, (10**strength[i]), True)
+                ratio[i, j-1] = result[4][Pixel]
+                para_num[j-1] = result[0].shape[0]
+        plt.clf()
+        im = plt.imshow(ratio, interpolation='bilinear', origin='lower', cmap=cm.gray, extent=(-3,3,-2,2))
+        plt.contourf(strength, para_num, ratio)
+        #plt.title('Number of stars:%d'%num)
+        plt.xlabel(r'Strength of Regularization($log \lambda$)')
+        plt.ylabel("Number of parameters")
+        plt.flag()
+        plt.colorbar(im, orientation='horizontal', shrink=0.8)
+        #plt.ylim(ymax=1.0)
+        plt.savefig('l2-num-ratio_pixel(3,4).png')
+
