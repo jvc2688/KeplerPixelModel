23fdc084ffcdf5e9e228dac1fcb0667a7b1f6b1d
Dun Wang <dw1519@nyu.edu>
2014-08-14 02:30:14 -0400

    exclude nearby stars

--- code/magNeighor/preprocess.py
+++ code/magNeighor/preprocess.py
@@ -3,14 +3,25 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import magNeighorFit as mnf
+import math
 
-def rms_filter(target_tpf, neighor_tpfs, num):
+def filter(target_tpf, neighor_tpfs, distance, rms=False, num=0):
     dtype = [('kic', int), ('rms', float), ('tpf', type(target_tpf)), ('lightcurve', np.ndarray)]
+    
+    time, target_flux, target_pixel_mask, target_kplr_mask, epoch_mask, flux_err, target_column, target_row= mnf.load_data(target_tpf)
 
     neighor_list = []
+    tpfs = {}
 
     for key, tpf in neighor_tpfs.items():
-        time, flux, pixel_mask, kplr_mask, epoch_mask, flux_err = mnf.load_data(tpf)
+        time, flux, pixel_mask, kplr_mask, epoch_mask, flux_err, column, row = mnf.load_data(tpf)
+        dis_square = (target_row-row)**2+(target_column-column)**2
+        if dis_square < distance**2:
+            continue
+        elif not rms:
+            tpfs[key] = tpf
+            continue
+        
         time = time[epoch_mask>0]
         flux = flux[epoch_mask>0]
         lightcurve = np.sum(flux, axis=1)
@@ -39,21 +50,22 @@ def rms_filter(target_tpf, neighor_tpfs, num):
 
         neighor_list.append((key, mean_std, tpf, lightcurve))
 
-    neighor_list = np.array(neighor_list, dtype=dtype)
-    neighor_list = np.sort(neighor_list, kind='mergesort', order='rms')
+    if rms:
+        neighor_list = np.array(neighor_list, dtype=dtype)
+        neighor_list = np.sort(neighor_list, order='rms')
 
-    tpfs = {}
-    for i in range(0, num):
-        tmp_kic, tmp_rms, tmp_tpf, tmp_lightcurve = neighor_list[i]
-        tpfs[tmp_kic] = tmp_tpf
-        '''
-        plt.plot(time, tmp_lightcurve, '.b', markersize=1)
-        plt.xlabel('time[BKJD]')
-        plt.ylabel('lightcurve')
-        plt.title('kic:%d quarter:%d rms:%f'%(tmp_kic, quarter, tmp_rms))
-        plt.savefig('./kic%d/preprocess/num%d.png'%(kid, i))
-        plt.clf()
-        '''
+        for i in range(0, num):
+            tmp_kic, tmp_rms, tmp_tpf, tmp_lightcurve = neighor_list[i]
+            tpfs[tmp_kic] = tmp_tpf
+            '''
+            plt.plot(time, tmp_lightcurve, '.b', markersize=1)
+            plt.xlabel('time[BKJD]')
+            plt.ylabel('lightcurve')
+            plt.title('kic:%d quarter:%d rms:%f'%(tmp_kic, quarter, tmp_rms))
+            plt.savefig('./kic%d/preprocess/num%d.png'%(kid, i))
+            plt.clf()
+            '''
+
     return tpfs
 
 
